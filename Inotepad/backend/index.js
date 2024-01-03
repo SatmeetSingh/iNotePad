854d4c58473d1399fe6d4d854414ea2f2f06417b
Satmeet_Singh <satmeetarora@gmail.com>
2024-01-03 18:28:23 +0530

    mongodb Connected

--- Inotepad/backend/index.js
+++ Inotepad/backend/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import connectToMongo from "./db.js";
 
+connectToMongo();
 const app = express();
+const port = 3000;
 
-app.get("/", () => {});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
