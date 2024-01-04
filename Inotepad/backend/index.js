732492068235db883cf9754d6b79bf1c6065cda1
Satmeet_Singh <satmeetarora@gmail.com>
2024-01-04 14:50:09 +0530

    Database

--- Inotepad/backend/index.js
+++ Inotepad/backend/index.js
@@ -1,12 +1,22 @@
 import express from "express";
 import connectToMongo from "./db.js";
+import authRouter from "../backend/routes/auth.js";
+import notesRouter from "../backend/routes/notes.js";
 
 connectToMongo();
 const app = express();
 const port = 3000;
 
+// middleware  for res.body data
+app.use(express.json());
+
+// Avialable Routes
+
+app.use("/api/auth", authRouter);
+app.use("/api/notes", notesRouter);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("hello");
 });
 
 app.listen(port, () => {
